de59f9a83b217f113511de73720664e896e082d6
Abhinav MV <mvabhinav1998@gmail.com>
2021-05-23 18:54:09 +0530

    Page feature Added

--- src/Components/PokemonCard.jsx
+++ src/Components/PokemonCard.jsx
@@ -1,50 +1,85 @@
 import React from 'react';
-// import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
-// import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Typography from '@material-ui/core/Typography';
 import { Grid } from '@material-ui/core';
+import {makeStyles} from '@material-ui/core/styles'
+import { capitalise } from '../utils/Capitalise';
+import { deepPurple,purple } from '@material-ui/core/colors';
 
-// const useStyles = makeStyles(()=>({
-//   root: {
-//     minWidth: 100,
-//     minHeight: 100,
-//     maxWidth: 100,
-//     maxHeight:100,
-//     alignText:'center',
-//   },
-//   Media: {
-//     height: 50,
-//     width: 50,
-//     objectFit: 'cover'
-//   }
-// }));
+const colours = {
+    'fire':'#F5AC78',
+    'water':'#9DB7F5',
+    'grass':'#A7DB8D',
+    'bug':'#C6D16E',
+    'fighting':'#D67873',
+    'ground':'#EBD69D',
+    'rock':'#D1C17D',
+    'ghost': '#A292BC' ,
+    'poison':purple[200],
+    'electric':'#FAE078',
+    'fairy':'#F4BDC9',
+    'psychic':'#FA92B2',
+    'normal':'#C6C6A7',
+    'flying':'#C6B7F5',
+    'dark':'#A29288',
+    'dragon':deepPurple[200],
+    'ice':'#BCE6E6',
+    'steel':'#D1D1E0'
+}
 
-export default function PokemonCard({name,id,url,history}) {
-  // const classes = useStyles();
 
-  return (
-    <Grid item xs={12} sm={6} md={4} lg={2} >
-        <Card style={{minHeight:250,minWidth:250}} >
-            <CardActionArea onClick={()=>(history.push(`/pokemon/${id}`))}>
-                <CardMedia
-                // className={classes.Media}
-                component="img"
-                alt={name}
-                // height="150"
-                image={url}
-                title={name}
-                />
-                <CardContent style={{textAlign:'center'}}>
-                    <Typography gutterBottom variant="h5" component="h2">
-                        {`${id}.${name}`}
-                    </Typography>
-                </CardContent>
-            </CardActionArea>
-        </Card>
-    </Grid>
-  );
+const useStyles = makeStyles(()=>({
+  Media: {
+    height: 200,
+    width: 200,
+    margin:'auto',
+    objectFit: 'cover'
+  },
+  container: props=>( {
+    height:'120',
+    width:'100',
+  }),
+  inner:props=>({
+    height:'220px',
+    width:'220px',
+    background:props.colour,
+    mozBorderRaidus: '100px',
+    webkitBorderRadius: '100px',
+    borderRadius: '100px',
+    margin:'auto'
+  })
+}));
+
+
+export const PokemonCard = ({name,id,types,weight,height,stats,url}) =>{
+    let colour = colours[types[0]]
+    const classes = useStyles({colour});
+    
+    return (
+        <Grid item xs={12} sm={6} md={4} lg={3} xl={2} >
+            <Card style={{width:250,height:310}} >
+                <CardActionArea>
+                    <div className={classes.container}>
+                    <CardMedia
+                    className={classes.inner}
+                    component="img"
+                    image={url.other['official-artwork'].front_default}
+                    title={name}
+                    />
+                    </div>
+                    <CardContent style={{textAlign:'center'}}>
+                        <Typography gutterBottom variant="h5" component="h2">
+                            {capitalise(name)}
+                        </Typography>
+                        <div style={{display:'inline-flex'}}>
+                          {types.map((type,index)=><Typography key={index} gutterBottom style={{background:colours[type],width:'70px',margin:2,borderRadius:'20px',marginBottom:4}} variant="subtitle1">{type}</Typography>)}
+                        </div>
+                    </CardContent>
+                </CardActionArea>
+            </Card>
+        </Grid>
+      );
 }
--- src/Containers/Pokedex.jsx
+++ src/Containers/Pokedex.jsx
@@ -1,40 +1,72 @@
 //files
-import { CircularProgress, Grid } from '@material-ui/core';
+import { Button, CircularProgress, Grid } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
-import axios from 'axios';
 import { useState } from 'react';
 import { useEffect } from 'react';
+import { Pagination } from '@material-ui/lab';
+
 import Appbar from '../Components/Appbar';
-import PokemonCard from '../Components/PokemonCard';
-import { capitalise } from '../utils/Capitalise';
+import { PokemonCard } from '../Components/PokemonCard';
 const useStyles = makeStyles((theme) => ({
     root: {
         display: 'flex',
         flexDirection: 'row',
         flexWrap: 'wrap',
+        margin:'auto',
+
     }
+
 }))
 const Pokedex = (props) => {
-    const history = props.history;
+    // const history = props.history;
     const classes = useStyles();
-    const [pokeData, setPokeData] = useState(false)
     const [search, setSearch] = useState('')
+    const [allPokemon,setAllPokemon] = useState([])
+    const [loadMore,setLoadMore] = useState('https://pokeapi.co/api/v2/pokemon?limit=100')
+    const [isLoading,setIsLoading] = useState(false)
+    
 
-    const handleRequest = async () => {
-        await axios.get('https://pokeapi.co/api/v2/pokemon?limit=898')
-            .then((response) => {
-                const { results } = response.data
-                console.log(response)
-                setPokeData(results)
-            }
-            )
-            .catch()
+    //---------------------------------------------------------------------------------------------
+    const [pageNumber,setPageNumber] = useState(0)
+    const pokemonsPerPage = 100
+    const pagesVisited = pageNumber * pokemonsPerPage
+    const noOfPages = Math.floor(1118/pokemonsPerPage)
 
+
+    //---------------------------------------------------------------------------------------------
+
+    const getType = (types) =>{
+        let type = []
+        types.map((obj)=>(
+            type.push(obj['type']['name'])
+        ))
+        return type
     }
-
+    const getAllPokemon = async () =>{
+        console.log('getallfunction')
+        setIsLoading(true)
+        const res = await fetch(loadMore)
+        const data = await res.json()
+        setLoadMore(data.next)
+        // count = data.count
+        function createPokemonObject(result){
+            result.forEach(async(pokemon)=>{
+                const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon.name}`)
+                const data = await res.json()
+                setAllPokemon(currentList => [...currentList,data])
+                // console.log(data)
+            })
+        }
+        createPokemonObject(data.results)
+        setIsLoading(false)
+    }
     useEffect(() => {
-        handleRequest()
-    }, [])
+        console.log('useefect')
+        getAllPokemon()
+    },[])
+    // console.log(allPokemon.slice(pagesVisited,pagesVisited+pokemonsPerPage))
+
+    const displayPokemons = allPokemon.slice(pagesVisited,pagesVisited+pokemonsPerPage).map((pokemon,index)=>(allPokemon[index].name.includes(search) && <PokemonCard key={index} name={pokemon.name} id={pokemon.id} types={getType(pokemon.types)} weight={pokemon.weight} height={pokemon.height} stats={pokemon.stats} url={pokemon.sprites}/>))
 
 
     return (
@@ -50,19 +82,15 @@ const Pokedex = (props) => {
                     alignContent='center'
                     wrap='wrap'
                     spacing={4}>
-
-                    {pokeData ?
-                        pokeData.map((obj, index) => {
-
-                            const { name, url: pokeUrl } = obj;
-                            let newName = capitalise(name)
-                            const url = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${index + 1}.png`;
-                            return pokeData[index].name.includes(search) && <PokemonCard key={index} name={newName} url={url} id={index + 1} history={history} pokeUrl={pokeUrl} />
+                        {
+                            isLoading?
+                            <CircularProgress style={{margin:'auto'}}/>:
+                            displayPokemons
                         }
-                        )
-                        : <CircularProgress style={{ margin: 'auto' }} />
-                    }
+                        
                 </Grid>
+                <Pagination count={noOfPages} page={pageNumber+1} onChange={(event,value)=>{getAllPokemon();setPageNumber(value-1)}} style={{width:'fit-content',margin:'auto'}}/>
+                {/* <Button >Load more</Button> */}
             </div>
         </>
     );
